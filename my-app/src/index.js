05daf059ba897e681a5413e6010dfe8297d9b4e8
Дастан <dastanjanybekov92@gmail.com>
2024-11-23 22:12:12 +0600

    nitial commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles/App.css';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App'; // Импорт компонента App
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App /> {/* Используем App здесь */}
+    </React.StrictMode>
+);
